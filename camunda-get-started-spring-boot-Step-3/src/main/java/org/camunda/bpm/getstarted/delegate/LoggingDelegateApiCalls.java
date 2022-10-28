c79a927230f8cfe9590f17811fc504ab26863804
roshinivr <roshinirajesh1@gmail.com>
2022-10-28 23:26:45 +0100

    Add onboarding2.bpmn and LoggingDelegateApiCalls

--- camunda-get-started-spring-boot-Step-3/src/main/java/org/camunda/bpm/getstarted/delegate/LoggingDelegateApiCalls.java
+++ camunda-get-started-spring-boot-Step-3/src/main/java/org/camunda/bpm/getstarted/delegate/LoggingDelegateApiCalls.java
@@ -1,10 +1,13 @@
 package org.camunda.bpm.getstarted.delegate;
 
+import org.camunda.bpm.engine.delegate.BpmnError;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.stereotype.Component;
 
 import java.util.logging.Logger;
+
+import static java.lang.System.Logger.Level.ERROR;
 
 @Component
 public class LoggingDelegateApiCalls implements JavaDelegate {
@@ -12,14 +15,20 @@ public class LoggingDelegateApiCalls implements JavaDelegate {
 
     @Override
     public void execute(DelegateExecution execution) throws Exception {
-        LOGGER.info("\n\n  ... logging invoked by "
-                + "processDefinitionId=" + execution.getProcessDefinitionId()
-                + ", activtyId=" + execution.getCurrentActivityId()
-                + ", activtyName='" + execution.getCurrentActivityName() + "'"
-                + ", processInstanceId=" + execution.getProcessInstanceId()
-                + ", businessKey=" + execution.getProcessBusinessKey()
-                + ", executionId=" + execution.getId()
-                + " \n\n");
-
+//        LOGGER.info("\n\n  ... logging invoked by "
+//                + "processDefinitionId=" + execution.getProcessDefinitionId()
+//                + ", activtyId=" + execution.getCurrentActivityId()
+//                + ", activtyName='" + execution.getCurrentActivityName() + "'"
+//                + ", processInstanceId=" + execution.getProcessInstanceId()
+//                + ", businessKey=" + execution.getProcessBusinessKey()
+//                + ", executionId=" + execution.getId()
+//                + " \n\n");
+        String businessKey = execution.getProcessBusinessKey();
+        if("success".equals(businessKey)){
+            LOGGER.info("Successfully completed " + execution.getCurrentActivityName());
+        }else if("fail".equals(businessKey)){
+            LOGGER.info( execution.getCurrentActivityName() + "failed!");
+            throw new BpmnError(execution.getCurrentActivityId() + "failed", "An error has occurred while making the api call. Please retry.");
+        }
     }
 }
